69409b7e24f549b893942a419136e5738fb55e8e
Ryan Batchelder <rb688@nau.edu>
2014-08-06 10:36:45 -0700

    Working out some of the quirks with authentication, almost got it working

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,36 +1,39 @@
 'use strict';
 
 app.factory('Auth',
-            function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope) {
+            function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope, $location) {
               var ref = new Firebase(FIREBASE_URL);
+              var userAuth = null;
 
-              var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+              var authTool = new FirebaseSimpleLogin(ref, function(error, user) {
                 if (error) {
                   console.log(error);
                 }
                 if (user) {
-                  $rootScope.user = user;
-                  console.log(user);
+                  userAuth = user;
+                  console.log(userAuth);
+                  $location.path('/');
                 }
                 else {
                   $rootScope.user = null;
                   console.log('No user logged in');
+                  $location.path('/');
                 }
               });
 
               var Auth = {
                 register: function (user) {
-                  return auth.$createUser(user.email, user.password);
+                  return authTool.createUser(user.email, user.password);
                 },
                 signedIn: function() {
-                  console.log($rootScope.user);
-                  return $rootScope.user !== null;
+                  console.log(userAuth);
+                  return userAuth !== null;
                 },
                 login: function(user) {
-                  return auth.login('password', user);
+                  return authTool.login('password', user);
                 },
                 logout: function() {
-                  auth.logout();
+                  authTool.logout();
                   console.log('Logged out');
                 }
               };
